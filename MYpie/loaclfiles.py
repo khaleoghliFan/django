66fab16338b41289bcb108a30c59e04b35321f33
kh <fanavakhaleoghli@gmail.com>
2025-02-14 23:20:15 +0330

    Add files via upload

---
+++ MYpie/loaclfiles.py
@@ -0,0 +1,5 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-#e1s(iz#n$v&21)$2&8=p1h&louw*f7ynu6xtfez0=mog3we=4'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
