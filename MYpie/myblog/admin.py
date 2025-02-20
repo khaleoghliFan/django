c732366adba4a3100e8cc758d4f7b7a36514f2ec
kh <fanavakhaleoghli@gmail.com>
2025-02-20 20:13:11 +0330

    Update admin.py

--- MYpie/myblog/admin.py
+++ MYpie/myblog/admin.py
@@ -1,14 +1,24 @@
 from django.contrib import admin
-from .models import Post,Item
-
+from .models import Post,Item,Author,Book
+from django.urls import reverse
 # Register your models here.
 @admin.register(Post)
 class PostModel(admin.ModelAdmin):
-    list_display = ('title','category')
+    list_display = ('pk','title','category')
     search_fields = ('title',)
     list_filter = ('category',)
 
 @admin.register(Item)
 class ItemsModel(admin.ModelAdmin):
-    list_display = ('name', 'category')
+    list_display = ('pk','name', 'category')
+
+@admin.register(Author)
+class AuthorModel(admin.ModelAdmin):
+    list_display = ('pk','name')
+
+@admin.register(Book)
+class BookModel(admin.ModelAdmin):
+    list_display = ('pk','author_name','title')
+
+
 
