3078a98be75c7d2393e0e96c8ce6cde19cd52d7d
kh <fanavakhaleoghli@gmail.com>
2025-02-20 20:10:15 +0330

    Update views.py

--- MYpie/myblog/views.py
+++ MYpie/myblog/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView,ListView
-from .models import Post,Item
+from django.shortcuts import render,get_object_or_404
+from django.views.generic.edit import CreateView
+from .models import Post,Item,Book,Author
+from django.urls import reverse_lazy
 
+class Create(CreateView):
+    model=Author
+    template_name = 'create.html'
+    fields = ["name"]
+    success_url = reverse_lazy('home')
 def home(request):
     posts = Post.objects.all()  # Fetch all posts
     return render(request, 'home.html', {'posts': posts})
@@ -10,9 +16,16 @@ def home(request):
 def blog(request):
     i=Item.objects.all()
     return(render(request,'blog.html',{'item':i}))
-    #template_name = 'blog.html'
 
-#class Home(TemplateView):
- #   template_name='home.html'
+def home_detail(request,pk):
+    model=get_object_or_404(Post,pk=pk)
+    return render(request,'extra.html',{"objects":model})
+def book(request):
+    book = Book.objects.all()  # Fetch all posts
+    return render(request, 'book.html', {'books': book})
+
+def book_detail(request, slug):
+    book = get_object_or_404(Book, slug=slug)
+    return render(request, 'book_detail.html', {'book': book})
 
 
