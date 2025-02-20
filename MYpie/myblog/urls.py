48e2fbd1e49e74139734aa9c6aadf6ab8823b3d6
kh <fanavakhaleoghli@gmail.com>
2025-02-20 20:12:29 +0330

    Update urls.py

--- MYpie/myblog/urls.py
+++ MYpie/myblog/urls.py
@@ -1,9 +1,15 @@
 #from .views import Blog
 from django.urls import path
-from .views import home,blog
+from .views import home,blog,home_detail,book,book_detail,Create
 
 
 urlpatterns = [
     path('blog/',blog ,name="blog"),
     path('home/', home, name='home'),
+    path('detail/<int:pk>/',home_detail,name="detail"),
+    path('book/', book, name='book'),
+    path('book/<slug:slug>/',book_detail,name='book_detail'),
+    path('create/',Create.as_view(),name='create')
+
+
 ]
